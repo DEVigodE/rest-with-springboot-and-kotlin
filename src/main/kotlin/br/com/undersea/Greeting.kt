02a72d67ee55242230aa40bd0b22ada81bf95525
ZIBMBRAZIL <moreira_igor9@hotmail.com>
2022-10-05 07:43:53 -0300

    Init

---
+++ src/main/kotlin/br/com/undersea/Greeting.kt
@@ -0,0 +1,3 @@
+package br.com.undersea
+
+class Greeting (val id: Long, val content: String)
---
+++ src/test/kotlin/br/com/undersea/StartupTests.kt
@@ -0,0 +1,13 @@
+package br.com.undersea
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
