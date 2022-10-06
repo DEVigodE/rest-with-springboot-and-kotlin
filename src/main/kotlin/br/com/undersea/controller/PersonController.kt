66acd65b48e740cfeb4eba68f48bbe0190df9afa
ZIBMBRAZIL <moreira_igor9@hotmail.com>
2022-10-06 06:09:59 -0300

    Implementando o Padrão de Projetos VO Adicionando os Testes do DozerMapper Ajustando o Service e o Controller

--- src/main/kotlin/br/com/undersea/controller/PersonController.kt
+++ src/main/kotlin/br/com/undersea/controller/PersonController.kt
@@ -1,9 +1,10 @@
 package br.com.undersea.controller
 
-import br.com.undersea.model.Person
+import br.com.undersea.data.vo.PersonVO
 import br.com.undersea.services.PersonService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -15,47 +16,42 @@ class PersonController {
     private lateinit var service: PersonService
     // var service: PersonService = PersonService()
 
-    @RequestMapping(
-        method = [RequestMethod.GET],
-        produces = [MediaType.APPLICATION_JSON_VALUE]
-    )
-    fun findAll(): List<Person> {
+    @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun findAll(): List<PersonVO> {
         return service.findAll()
     }
 
-    @RequestMapping(
-        value = ["/{id}"], method = [RequestMethod.GET],
+    @GetMapping(
+        value = ["/{id}"],
         produces = [MediaType.APPLICATION_JSON_VALUE]
     )
-    fun findById(@PathVariable(value = "id") id: Long): Person {
+    fun findById(@PathVariable(value = "id") id: Long): PersonVO {
         return service.findById(id)
     }
 
-    @RequestMapping(
-        method = [RequestMethod.POST],
+    @PostMapping(
         consumes = [MediaType.APPLICATION_JSON_VALUE],
         produces = [MediaType.APPLICATION_JSON_VALUE]
     )
-    fun create(@RequestBody person: Person): Person {
+    fun create(@RequestBody person: PersonVO): PersonVO {
         return service.create(person)
+
     }
 
-    @RequestMapping(
-        method = [RequestMethod.PUT],
+    @PutMapping(
         consumes = [MediaType.APPLICATION_JSON_VALUE],
         produces = [MediaType.APPLICATION_JSON_VALUE]
     )
-    fun update(@RequestBody person: Person): Person {
+    fun update(@RequestBody person: PersonVO): PersonVO {
         return service.update(person)
     }
 
-    @RequestMapping(
-        value = ["/{id}"], method = [RequestMethod.DELETE],
+    @DeleteMapping(
+        value = ["/{id}"],
         produces = [MediaType.APPLICATION_JSON_VALUE]
     )
-    fun delete(@PathVariable(value = "id") id: Long) {
+    fun delete(@PathVariable(value = "id") id: Long): ResponseEntity<*> {
         service.delete(id)
+        return ResponseEntity.noContent().build<Any>()
     }
-
-
 }
---
+++ src/main/kotlin/br/com/undersea/data/vo/PersonVO.kt
@@ -0,0 +1,9 @@
+package br.com.undersea.data.vo
+
+data class PersonVO(
+    var id: Long = 0,
+    var firstName: String = "",
+    var lastName: String = "",
+    var address: String = "",
+    var gender: String = ""
+)
