e37c5ad961164b179dbbb6b0ae5a10251edb02f4
ZIBMBRAZIL <moreira_igor9@hotmail.com>
2022-10-06 10:39:03 -0300

    Adicionando Suporte à Migrations com Flyway Custom JSON Serialization Implementando HATEOAS (Hypermedia as the Engine of Application State)

---
+++ src/main/kotlin/br/com/undersea/data/vo/PersonVO.kt
@@ -0,0 +1,24 @@
+package br.com.undersea.data.vo
+
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
+import com.github.dozermapper.core.Mapping
+import org.springframework.hateoas.RepresentationModel
+
+@JsonPropertyOrder("id", "address", "first_name", "last_name", "gender")
+data class PersonVO(
+
+    @Mapping("id")
+    var key: Long = 0,
+
+    @field:JsonProperty("first_name")
+    var firstName: String = "",
+
+    @field:JsonProperty("last_name")
+    var lastName: String = "",
+    var address: String = "",
+
+    @field:JsonIgnore
+    var gender: String = ""
+) : RepresentationModel<PersonVO>()
